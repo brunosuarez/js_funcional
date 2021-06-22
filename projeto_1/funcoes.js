273a6e9a144ec3ecc25dc0a20d0b52f1612c5aa9
bruno.soares <bruno.soares@db1.com.br>
2021-06-22 11:36:13 -0300

    avancando no modulo de palavras mais usadas

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -29,13 +29,48 @@ function lerArquivos(caminhos){
     return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padrao){
-    return array.filter(el => el.endsWith(padrao))
+function elementosTerminadosCom(padraoTextual){
+    return function(array){
+        return array.filter(el => el.endsWith(padraoTextual))
+    }
+}
+
+function removerElementosSeVazio(array){
+    return array.filter(el => el.trim())
+}
+
+function removerElementosSeIncluir(padraoTextual){
+    return function(array){
+        return array.filter(el => el.includes(padraoTextual))
+    }
+}
+
+function removerElementosSeApenasNumero(array){
+    return array.filter(el => {
+        const num = parseInt(el.trim())
+        return num !== num
+    })
+}
+
+function removerSimbolos(simbolos){
+    return function(array){
+        return array.map(el => {
+            let textSemSimbolos = el
+            simbolos.forEach(simbolo => {
+                textSemSimbolos = textSemSimbolos.split(simbolo).join('')
+            })
+            return textSemSimbolos
+        })
+    }
 }
 
 module.exports = {
     lerDiretorio,
-    elementosTerminadosCom,
     lerArquivo,
-    lerArquivos
+    lerArquivos,
+    elementosTerminadosCom,
+    removerElementosSeVazio,
+    removerElementosSeIncluir,
+    removerElementosSeApenasNumero,
+    removerSimbolos
 }
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -3,9 +3,18 @@ const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
+const simbolos = [
+    '.', '?', '-', ',', '"', '_', '<i>', '</i>', '\r', '[', '],', '(',')'
+]
+
 fn.lerDiretorio(caminho)
-    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+    .then(fn.elementosTerminadosCom('.srt'))
     .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
     .then(conteudos => conteudos.join(''))
     .then(todoConteudo => todoConteudo.split('\n'))
+    .then(fn.removerElementosSeVazio)
+    .then(linhas => fn.removerElementosSeVazio(linhas))
+    .then(fn.removerElementosSeIncluir('-->'))
+    .then(fn.removerElementosSeApenasNumero)
+    .then(fn.removerSimbolos(simbolos))
     .then(console.log)
